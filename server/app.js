3819cf266421e98b0b439dd21f0bde064e65f6e4
Daniyal Asif <daniyalasif550@gmail.com>
2026-04-28 10:04:49 +0530

    cors error

--- server/app.js
+++ server/app.js
@@ -4,7 +4,6 @@ dotenv.config()
 import express from "express"
 import mongoose from "mongoose"
 import cors from "cors"
-
 import cookieParser from "cookie-parser"
 
 import signUpRoute from "./routes/signup.js"
@@ -12,59 +11,45 @@ import loginRoute from "./routes/login.js"
 import userRoutes from './routes/user.js'
 import BookingRoute from './routes/bookings.js'
 
+const app = express()
+app.use(express.json())
+app.use(cookieParser())
+
+const isProduction = process.env.NODE_ENV === "production"
+
 const allowedOrigins = [
     "http://localhost:5173",
     "http://localhost:5174",
     "http://localhost:3000",
     'https://urban-ease-steel.vercel.app',
     'https://urban-ease-daniyal-asifs-projects-2b05fd7a.vercel.app',
-];
+]
 
 app.use((req, res, next) => {
-    res.setHeader("Cross-Origin-Opener-Policy", "same-origin-allow-popups");
-    next();
-});
-
+    res.setHeader("Cross-Origin-Opener-Policy", "same-origin-allow-popups")
+    next()
+})
 
 app.use(cors({
     origin: function (origin, callback) {
-
-        if (!origin) return callback(null, !isProduction);
+        if (!origin) return callback(null, !isProduction)
         if (allowedOrigins.indexOf(origin) === -1) {
-            const msg = 'The CORS policy for this site does not allow access from the specified Origin.';
-            return callback(new Error(msg), false);
+            return callback(new Error('CORS policy does not allow this origin.'), false)
         }
-        else {
-            return callback(null, true);
-        }
+        return callback(null, true)
     },
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
     allowedHeaders: ["Content-Type", "Authorization"],
-}));
-
-
-const app = express()
-app.use(express.json())
-
-app.use(cookieParser())
+}))
 
 const PORT = process.env.PORT || 5000
 const HOST = '0.0.0.0'
-
 const MONGO_URL = process.env.ATLAS_DB_URL || 'mongodb://127.0.0.1:27017/urban'
 
-const connectDB = async () => {
-    await mongoose.connect(MONGO_URL)
-}
-
-connectDB()
-    .then(async () => {
-        console.log("Connected To MongoDB")
-    })
-    .catch((err) => {
-        console.log(err)
-    })
+mongoose.connect(MONGO_URL)
+    .then(() => console.log("Connected To MongoDB"))
+    .catch((err) => console.log(err))
 
 app.use('/api/signup', signUpRoute)
 app.use('/api/login', loginRoute)
